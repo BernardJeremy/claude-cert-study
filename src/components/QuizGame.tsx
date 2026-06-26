643d39f2b601da5200b1ff78d676c2a2e3842876
Jeremy Bernard <jeremy@bernard.sh>
2026-06-26 07:26:55 +0200

    feat: add question count print

--- src/components/QuizGame.tsx
+++ src/components/QuizGame.tsx
@@ -31,6 +31,7 @@ export default function QuizGame({ questions }: Props) {
 
   useEffect(() => {
     setDeck(shuffle(questions))
+    console.log(`${questions.length} questions loaded.`)
   }, [questions])
 
   const current = deck[index]
